2dce9412e1561755398ab85915c43a91e885c1b9
siddharth raghav <thakurraghav27@gmail.com>
2022-07-19 12:58:26 +0530

    complete

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,92 @@
 'use strict';
+
+//selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let score, currentScore, activePlayer, play;
+
+//starting
+const init = function () {
+  score0El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  score = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  play = true;
+};
+init();
+
+const changeplayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// rollign dice functionality
+
+btnRoll.addEventListener('click', function () {
+  if (play) {
+    //1.genrate random no
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. check for rolled if true switch next player
+    if (dice !== 1) {
+      //add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch to next player
+      changeplayer();
+    }
+  }
+});
+
+// hold function
+
+btnHold.addEventListener('click', function () {
+  if (play) {
+    score[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    if (score[activePlayer] >= 20) {
+      play = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      changeplayer();
+    }
+  }
+  //   switching playe/
+});
+
+// reseting game?
+btnNew.addEventListener('click', init);
